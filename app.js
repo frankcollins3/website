dace6b378a3d0fd1c50494df31425a8f06831276
MusicMan <musicman@MacBook-Pro.fios-router.home>
2020-10-28 00:35:39 -0400

    [website] added bootstrap components

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const pics = document.querySelectorAll('img');
+pics.addEventListener('onmouseover', function(event) {
+    console.log(alert("WOW"));
+    img.style.background-color= "red";
+
+
+
